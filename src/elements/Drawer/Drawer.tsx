170f433e98497f5ad2f034f77a2e8ffac4141053
Jakob Sawal <jakob.sawal@ts.berlin>
2026-02-18 14:29:25 +0100

    fix: mobile menu width

--- src/elements/Drawer/Drawer.tsx
+++ src/elements/Drawer/Drawer.tsx
@@ -38,9 +38,9 @@ const drawerVariants = cva(
 				top: "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top",
 				bottom:
 					"inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
-				left: "inset-y-0 left-0 h-full w-[35rem] border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left",
+				left: "inset-y-0 left-0 h-full w-[35rem] max-w-[100vw] border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left",
 				right:
-					"inset-y-0 right-0 h-full w-[35rem] border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right",
+					"inset-y-0 right-0 h-full w-[35rem] max-w-[100vw] border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right",
 			},
 			size: {
 				default: "",
